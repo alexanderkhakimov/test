2f53e5169873c8acef3019481eb2b25a208085d8
Alex Khakimov <alexanderkhakimov@gmail.com>
2023-09-07 13:19:14 +0300

    First commit

---
+++ Node.java
@@ -0,0 +1,44 @@
+package BinaryTree;
+
+public class Node {
+    private int value;
+    public Node leftChild;
+    public Node rightChild;
+
+    public void printNode(){
+        System.out.println(" Выбранный узел имеет значение :"+ value);
+    }
+
+    public int getValue() {
+        return this.value;
+    }
+
+    public void setValue(final int value) {
+        this.value = value;
+    }
+
+    public Node getLeftChild() {
+        return leftChild;
+    }
+
+    public void setLeftChild(Node leftChild) {
+        this.leftChild = leftChild;
+    }
+
+    public Node getRightChild() {
+        return rightChild;
+    }
+
+    public void setRightChild(Node rightChild) {
+        this.rightChild = rightChild;
+    }
+
+    @Override
+    public String toString() {
+        return "Node{" +
+                "value=" + value +
+                ", leftChild=" + leftChild +
+                ", rightChild=" + rightChild +
+                '}';
+    }
+}
